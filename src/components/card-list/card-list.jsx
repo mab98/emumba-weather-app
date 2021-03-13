da79981fbd244628b8bea1d975fad2869ec3c29d
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-03-13 15:13:22 +0500

    changed card-list logic

--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -4,14 +4,21 @@ import './card-list.css'
 
 export const CardList = ({ weatherData, setLocation, setWeatherDay }) => (
   <div className='card-list'>
-    {weatherData.list.slice(0, 5).map(weather => (
-      <Card
-        key={weather.dt}
-        weather={weather}
-        weatherData={weatherData}
-        setLocation={setLocation}
-        setWeatherDay={setWeatherDay}
-      />
-    ))}
+    {weatherData.list
+      .reduce((acc, element) => {
+        if (element.dt_txt.includes('12:00:00')) {
+          acc.push(element)
+        }
+        return acc
+      }, [])
+      .map(weather => (
+        <Card
+          key={weather.dt}
+          weather={weather}
+          weatherData={weatherData}
+          setLocation={setLocation}
+          setWeatherDay={setWeatherDay}
+        />
+      ))}
   </div>
 )
